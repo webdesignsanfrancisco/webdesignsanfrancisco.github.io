bcb9f24131fd67f34594c2878876b3abe4fadfac
derfr <frr@users.noreply.github.com>
2021-02-25 15:19:28 +0100

    new nav + fixes

--- _site/script.js
+++ _site/script.js
@@ -12,7 +12,17 @@ labels.forEach(label => {
 
 
 
+const nav = document.querySelector('.nav')
+window.addEventListener('scroll', fixNav)
 
 
+
+function fixNav() {
+    if(window.scrollY > nav.offsetHeight) {
+        nav.classList.add('active')
+    } else {
+        nav.classList.remove('active')
+    }
+}
 
 
